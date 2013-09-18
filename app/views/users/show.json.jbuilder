588f1ec40caeae9b3ce5a360cc1a58f28506d66d
xiagit000 <411602796@qq.com>
2013-09-17 20:43:30 -0700

    add templet

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :account, :password, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 MyBlog::Application.routes.draw do
+resources:users
+root:to=>"users#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
